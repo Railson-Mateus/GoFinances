c3ad8a499f7589187307608b2f46205e04bce89e
Railson Mateus <raylsomatheus@hotmail.com>
2023-05-22 07:47:25 -0300

    feat: Create screen Resume

--- src/screens/Resume/index.tsx
+++ src/screens/Resume/index.tsx
@@ -1,11 +1,26 @@
-import { Text, View } from "react-native";
+import { ScrollView, View } from "react-native";
+
+import Header from "../../components/Header";
+import DateCarousel from "../../components/DateCarousel";
+import CardCategory from "../../components/CardCategory";
 
 import { styles } from "./styles";
 
 const Resume = () => {
   return (
-    <View>
-      <Text>Resume</Text>
+    <View style={styles.container}>
+      <Header title="Resumo por categoria" />
+      <DateCarousel />
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        decelerationRate={0.5}
+        style={styles.scroll}
+      >
+        <CardCategory category="Casa" value="1.200" />
+        <CardCategory category="Carro" value="700" />
+        <CardCategory category="Alimentação" value="900" />
+        <CardCategory category="Escola" value="500" />
+      </ScrollView>
     </View>
   );
 };
--- src/screens/Resume/styles.ts
+++ src/screens/Resume/styles.ts
@@ -2,5 +2,12 @@ import { StyleSheet } from "react-native";
 import THEME from "../../global/styles/theme";
 
 export const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    backgroundColor: THEME.colors.background,
+  },
+  scroll: {
+    flex: 1,
+  },
 });
